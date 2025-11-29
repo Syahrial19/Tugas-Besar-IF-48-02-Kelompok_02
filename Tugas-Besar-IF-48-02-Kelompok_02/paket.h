62adc00f3170e4bff52893bcb808e45b5be19cac
audyyla <afrilicia.audila@gmail.com>
2025-11-30 00:28:32 +0700

    Update paket.h

--- Tugas-Besar-IF-48-02-Kelompok_02/paket.h
+++ Tugas-Besar-IF-48-02-Kelompok_02/paket.h
@@ -1,6 +1,28 @@
 #ifndef PAKET_H_INCLUDED
 #define PAKET_H_INCLUDED
 
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct Kurir;
+struct Paket {
+    string idPaket;
+    string namaPenerima;
+    string alamat;
+    Paket* next;
+    Kurir* parent;
+};
+typedef Paket* adrPaket;
+struct ListPaket {
+    adrPaket first;
+};
 
-#endif // PAKET_H_INCLUDED
+void createListPaket(ListPaket &L);
+adrPaket createPaket(string idPaket, string namaPenerima, string alamat);
+void addPaketToKurir(Kurir* K, adrPaket P);
+adrPaket findPaketInKurir(Kurir* K, string idPaket);
+void deletePaketFromKurir(Kurir* K, string idPaket);
+void showPaketOfKurir(Kurir* K);
+
+#endif
