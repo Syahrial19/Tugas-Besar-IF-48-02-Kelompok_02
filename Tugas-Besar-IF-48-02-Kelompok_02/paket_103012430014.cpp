38d22e62f8d80ec2a532301c1c5c30a7dd1f6aa3
Syahrial <syahrialaffansyah@gmail.com>
2025-12-12 13:15:52 +0700

    Update paket_103012430014.cpp

--- Tugas-Besar-IF-48-02-Kelompok_02/paket_103012430014.cpp
+++ Tugas-Besar-IF-48-02-Kelompok_02/paket_103012430014.cpp
@@ -1,24 +1,102 @@
-void createListPaket(ListPaket &L) {
-    L.first = nullptr;
+#include "header.h"
+#include <iostream>
+using namespace std;
+
+void deleteFirstPaket(addressK K, addressP &P) {
+    if (!isEmptyPaket(K)) {
+        P = K->firstP;
+        K->firstP = P->next;
+        P->next = nullptr;
+    }
+}
+void deleteLastPaket(addressK K, addressP &P) {
+    if (!isEmptyPaket(K)) {
+
+        if (K->firstP->next == nullptr) {
+            deleteFirstPaket(K, P);
+        } else {
+            addressP Q = K->firstP;
+
+            while (Q->next->next != nullptr) {
+                Q = Q->next;
+            }
+
+            P = Q->next;
+            Q->next = nullptr;
+            P->next = nullptr;
+        }
+    }
+}
+void deleteAfterPaket(addressK K, addressP prec, addressP &P) {
+    if (prec != nullptr && prec->next != nullptr) {
+        P = prec->next;
+        prec->next = P->next;
+
+        P->next = nullptr;
+    }
 }
 
-adrPaket createPaket(string idPaket, string namaPenerima, string alamat) {
-    adrPaket P = new Paket;
-    P->idPaket = idPaket;
-    P->namaPenerima = namaPenerima;
-    P->alamat = alamat;
+adrKurir findPaket(ListKurir L, string idpaket){
+    adrP P = L.first;
 
-    P->next = nullptr;
-    P->parent = nullptr;
+    while (P != nullptr){
+        if (P->info.idpaket == idpaket){
+            return P;
+        }
+        P = P->next;
+    }
 
-    return P;
+    return ;
 }
-void addPaketToKurir(Kurir* K, adrPaket P) {
-    if (K == nullptr || P == nullptr)
-        return nullptr;
 
-    P->next = K->childHead;
-    P->parent = K;
-    K->childHead = P;
+void showAllPaket(addressK K) {
+    if (isEmptyPaket(K)) {
+        cout << "   (Tidak ada paket)\n";
+        return;
+    }
+
+    addressP P = K->firstP;
+    while (P != nullptr) {
+        cout << "   - ID Paket: " << P->info.idPaket
+             << ", Nama Penerima: " << P->info.namaPenerima
+             << ", Alamat: " << P->info.alamat << endl;
+        P = P->next;
+    }
 }
+
+int countPaket(addressK K) {
+    int count = 0;
+    addressP P = K->firstP;
+
+    while (P != nullptr) {
+        count++;
+        P = P->next;
+    }
+    return count;
+}
+void showPaketOfKurir(addressK K) {
+    if (K == nullptr) {
+        cout << "Kurir tidak ditemukan." << endl;
+        return;
+    }
+
+    cout << "== Daftar Paket Kurir ==" << endl;
+    cout << "Kurir : " << K->info.nama
+         << " (ID: " << K->info.id << ")" << endl;
+
+    addressP P = K->firstP;
+
+    if (P == nullptr) {
+        cout << "  Tidak ada paket." << endl;
+    } else {
+        while (P != nullptr) {
+            cout << "  - ID Paket: " << P->info.idPaket
+                 << ", Penerima: " << P->info.namaPenerima
+                 << ", Alamat: " << P->info.alamat
+                 << endl;
+            P = P->next;
+        }
+    }
+}
+
 
