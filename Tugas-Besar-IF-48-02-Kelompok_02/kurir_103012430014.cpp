622faf9bb72a60e50762abe7e510b0b07c4bcb78
Syahrial <syahrialaffansyah@gmail.com>
2025-12-12 13:16:27 +0700

    Update kurir_103012430014.cpp

--- Tugas-Besar-IF-48-02-Kelompok_02/kurir_103012430014.cpp
+++ Tugas-Besar-IF-48-02-Kelompok_02/kurir_103012430014.cpp
@@ -1,27 +1,57 @@
+#include "header.h"
+#include <iostream>
+using namespace std;
+
 void createListKurir(ListKurir &L){
     L.first = nullptr;
     L.last = nullptr;
 }
 
-adrKurir createKurir(string id, string nama, int umur) {
-    adrKurir K = new Kurir;
-    K->id = id;
-    K->nama = nama;
-    K->umur = umur;
-    K->childHead = nullptr;
-
+addressK createKurir(string id, string nama, int umur) {
+    addressK K = new elmKurir;
+    K->info.id = id;
+    K->info.nama = nama;
+    K->info.umur = umur;
+    K->firstP = nullptr;
     K->next = nullptr;
     K->prev = nullptr;
 
     return K;
 }
-void insertKurir(ListKurir &L, adrKurir P) {
+
+
+void insertFirstKurir(ListKurir &L, addressK K) {
     if (L.first == nullptr) {
-        L.first = P;
-        L.last = P;
+        L.first = K;
+        L.last = K;
     } else {
-        L.last->next = P;
-        P->prev = L.last;
-        L.last = P;
+        K->next = L.first;
+        L.first->prev = K;
+        L.first = K;
+    }
+}
+
+void insertLastKurir(ListKurir &L, addressK K) {
+    if (L.first == nullptr) {
+        L.first = K;
+        L.last = K;
+    } else {
+        L.last->next = K;
+        K->prev = L.last;
+        L.last = K;
+    }
+}
+
+void insertAfterKurir(ListKurir &L, addressK prec, addressK K) {
+    if (prec != nullptr) {
+        K->next = prec->next;
+        K->prev = prec;
+
+        if (prec->next != nullptr) {
+            prec->next->prev = K;
+        } else {
+            L.last = K;
+        }
+        prec->next = K;
     }
 }
