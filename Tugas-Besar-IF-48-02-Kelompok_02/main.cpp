6604f48e16585468dcb5b898ca255aa9e201317e
Syahrial <syahrialaffansyah@gmail.com>
2025-12-13 19:18:46 +0700

    Update main.cpp

--- Tugas-Besar-IF-48-02-Kelompok_02/main.cpp
+++ Tugas-Besar-IF-48-02-Kelompok_02/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include "main.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int option = -99;
+
+    while (option != 0) {
+        system("cls");
+        cout << "============ Menu ============\n";
+        cout << "|| 1. Test Case             ||\n";
+        cout << "|| 2. Study Case            ||\n";
+        cout << "|| 0. Exit                  ||\n";
+        cout << "==============================\n";
+        cout << "Choose your option : ";
+        cin >> option;
+
+        switch(option) {
+            case 1:
+                cout << "Kamu Memilih Test Case\n";
+                menuAdmin();   // ADMIN / TEST CASE
+                break;
+
+            case 2:
+                cout << "Kamu Memilih Study Case\n";
+                menuUser();    // USER / STUDY CASE
+                break;
+
+            case 0:
+                cout << "Terima kasih.\n";
+                break;
+
+            default:
+                cout << "Pilihan tidak valid.\n";
+                system("pause");
+                break;
+        }
+    }
+
     return 0;
 }
